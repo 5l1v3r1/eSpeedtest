ac15b7147757885c521c5e79d46342e797064b81
HKLCF <hklcf@users.noreply.github.com>
2015-11-28 04:07:22 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,3 +2,5 @@
 
 DL_SPEED=$(./download.sh | grep '[0-9] kB/s' | sed 's/^.*(//g' | sed 's/ *kB.*//g')
 UL_SPEED=$(./upload.sh | grep '[0-9] kB/s' | sed 's/^.*(//g' | sed 's/ *kB.*//g')
+
+curl 'https://docs.google.com/forms/d/<form_id>/formResponse' -H 'user-agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' --data 'entry.<q_id>='$DL_SPEED'&entry.<q_id>='$UL_SPEED' --compressed > /dev/null 2>&1
