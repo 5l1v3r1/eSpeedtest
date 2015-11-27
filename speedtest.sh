2a4f7356a6914f7ba01e2ec9ae4faa9572f794e8
HKLCF <hklcf@users.noreply.github.com>
2015-11-28 03:55:00 +0800

    Update speedtest.sh
    typo

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 DL_SPEED=$(./download.sh | grep '[0-9] kB/s' | sed 's/^.*(//g' | sed 's/ *kB.*//g')
-UL_SPEED-$(./upload.sh | grep '[0-9] kB/s' | sed 's/^.*(//g' | sed 's/ *kB.*//g')
+UL_SPEED=$(./upload.sh | grep '[0-9] kB/s' | sed 's/^.*(//g' | sed 's/ *kB.*//g')
